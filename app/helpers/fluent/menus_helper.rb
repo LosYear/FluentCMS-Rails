fb18c85b7873a5fe3319a27d55d080bc549f78f3
Losev Yaroslav <losevyaroslav@gmail.com>
2016-08-20 23:08:25 +0300

    Small fixes

--- app/helpers/fluent/menus_helper.rb
+++ app/helpers/fluent/menus_helper.rb
@@ -6,7 +6,7 @@ module Fluent::MenusHelper
   def render_menu menu, options = {}
     menu = Fluent::Menu.find_by(:name => menu).tree(:status => true)
 
-    body = _render_menu menu
+    body = _render_menu menu, options
     if options.has_key? :class
       content_tag :ul, body, :class => options[:class]
     else
@@ -15,15 +15,17 @@ module Fluent::MenusHelper
   end
 
   private
-  def _render_menu menu
+  def _render_menu menu, options
     menu.map do |key, item|
+      data_push = item[:data_push]
+      data_push = options[:data_push] if options.has_key? :data_push
       if item.has_key? :items
         content_tag :li do
-          ((link_to item[:title], item[:url], :data => {:push => item[:data_push]}).html_safe + content_tag(:ul, _render_menu(item[:items])).html_safe).html_safe
+          ((link_to item[:title], item[:url], :data => {:push => data_push}).html_safe + content_tag(:ul, _render_menu(item[:items])).html_safe).html_safe
         end
       else
         content_tag :li do
-          (link_to item[:title], item[:url], :data => {:push => item[:data_push]}).html_safe
+          (link_to item[:title], item[:url], :data => {:push => data_push}).html_safe
         end
       end
     end.join.html_safe
