a8bf4c211bd4a44429eca167f5a1a68a0c188518
Losev Yaroslav <losevyaroslav@gmail.com>
2016-10-06 22:31:28 +0300

    Node presence fix

--- app/models/fluent/node.rb
+++ app/models/fluent/node.rb
@@ -1,9 +1,5 @@
 module Fluent
   class Node < ApplicationRecord
-    # Relations
-    belongs_to :author, :foreign_key => "author", :class_name => "User"
-    belongs_to :updater, :foreign_key => "updater", :class_name => "User"
-
     # Validation
     validates :slug, presence: true, uniqueness: { case_sensitive: false }
     validates :title, presence: true
